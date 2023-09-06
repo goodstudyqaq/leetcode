f6bfa71c425142e1443a8ee8446c922537614d4f
shen <goodstudyQAQ@163.com>
2023-09-06 12:05:48 +0800

    [problem] basic-calculator-iv

---
+++ problems-cpp/basic-calculator-iv/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+cerr << "Case " << to_string(i + 1) + " testing..." << endl;
+        // string expression;
+        // convert_params(data[i * one_test_number + 0], expression);
+        // vector<string> evalvars;
+        // convert_params(data[i * one_test_number + 1], evalvars);
+        // vector<int> evalints;
+        // convert_params(data[i * one_test_number + 2], evalints);
+        // vector<string> real_res;
+        // c
